108cb472a055271dfcc4507d405f9648afc09bd6
Miguel <miguelgsilverios@gmail.com>
2022-03-12 05:14:34 -0300

    - Criação da tabela LimiteCliente, APICurso consegue ler os dados do banco, retornar um Limite de crédito específico e decrementar o limite de crédito.

---
+++ APICliente/Models/VendasCliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICliente.Models
+{
+    public class VendasCliente
+    {
+        public int VendaId { get; set; }
+
+        public int LimiteId { get; set; }
+        public DateTime DataVenda { get; set; }
+        public float Venda { get; set; }
+    }
+}
--- APICurso/Controllers/LimiteClienteController.cs
+++ APICurso/Controllers/LimiteClienteController.cs
@@ -1,6 +1,8 @@
 ﻿using APICurso.Context;
 using APICurso.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,12 +28,32 @@ namespace APICurso.Controllers
         }
 
         // Retorna o Limite de crédito de um cliente específico de acordo com o Id
-        [HttpGet("{id}")]
+        [HttpGet("Limite/{id}")]
         public ActionResult<float> Get(int id)
         {
+
             var cliente = _context.LimiteClientes.FirstOrDefault(x => x.LimiteId == id);
+            if (cliente == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Erro ao tentar obter os dados. Tente outro Id que seja válido.");
+            }
+
             return cliente.LimiteCredito;
         }
 
+        // Decrementa o Limite de crédito do cliente com um valor especificado
+        [HttpPatch("Limite/{id}/Decrementa/{valor}")]
+        public ActionResult<LimiteCliente> DecrementaCredito(int id,float valor)
+        {
+            var cliente = _context.LimiteClientes.FirstOrDefault(x => x.LimiteId == id);
+
+            cliente.LimiteCredito -= valor;
+
+            _context.Entry(cliente).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
